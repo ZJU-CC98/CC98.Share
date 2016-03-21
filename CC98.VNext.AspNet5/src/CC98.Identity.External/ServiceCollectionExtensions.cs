898a38fbe933a63f7d3f3c43efd565570eba8daf
Iris Sakura <sgjsakura@hotmail.com>
2016-03-21 15:26:15 +0800

    ASP.NEt 5 迁移

---
+++ CC98.VNext.AspNet5/src/CC98.Identity.External/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CC98.Identity.External;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+		/// <summary>
+		/// 为应用程序添加 CC98 登录身份验证服务。
+		/// </summary>
+		/// <param name="services">服务集合对象。</param>
+	    public static void AddCC98SignManager(this IServiceCollection services)
+	    {
+			services.TryAddScoped<ExternalSignInManager>();
+	    }
+    }
+}

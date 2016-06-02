94fefd580a831806e6a2cba8324ee814323546e8
Iris Sakura <sgjsakura@hotmail.com>
2016-06-02 14:02:11 +0800

    更改 IdentityCore 提供头像

--- CC98.VNext.AspNetCore/src/CC98.Identity.Core/CC98IdentityHelper.cs
+++ CC98.VNext.AspNetCore/src/CC98.Identity.Core/CC98IdentityHelper.cs
@@ -40,6 +40,21 @@ namespace CC98.Identity
         }
 
         /// <summary>
+        ///     获取指定用户凭据包含的用户头像地址。
+        /// </summary>
+        /// <param name="identity">用户凭据。</param>
+        /// <returns>用户的头像地址。</returns>
+        public static string GetPortraitUri(this ClaimsIdentity identity)
+        {
+            if (identity == null)
+            {
+                throw new ArgumentNullException(nameof(identity));
+            }
+
+            return identity.FindFirst(CC98UserClaimTypes.PortraitUri).Value;
+        }
+
+        /// <summary>
         ///     获取指定用户凭据包含的用户 ID 值。
         /// </summary>
         /// <param name="principal">用户凭据。</param>
@@ -67,6 +82,21 @@ namespace CC98.Identity
             }
 
             return int.Parse(principal.FindFirst(CC98UserClaimTypes.OldId).Value, CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        ///     获取指定用户凭据包含的用户头像地址。
+        /// </summary>
+        /// <param name="principal">用户凭据。</param>
+        /// <returns>用户的头像地址。</returns>
+        public static string GetPortraitUri(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentNullException(nameof(principal));
+            }
+
+            return principal.FindFirst(CC98UserClaimTypes.PortraitUri).Value;
         }
 
         /// <summary>

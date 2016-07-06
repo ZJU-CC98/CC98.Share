aba413ec1965a6b67fb3fe42cfcc09cd3b641002
Iris Sakura <sgjsakura@hotmail.com>
2016-07-06 17:48:22 +0800

    更新项目结构

---
+++ CC98.Share/src/CC98.Share/Migrations/20160613123315_total.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace cc98.share.Migrations
+{
+    public partial class total : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalSize",
+                table: "Items");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "TotalSize",
+                table: "Items",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
